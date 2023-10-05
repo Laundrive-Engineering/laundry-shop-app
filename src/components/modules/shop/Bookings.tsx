fa5239ecaa897c2907477b108f92e3bb80533ca9
Jojie Jagonos <jojie.jagonos23@gmail.com>
2023-10-05 13:01:37 +0800

    fix error in shop bookings

--- src/components/modules/shop/Bookings.tsx
+++ src/components/modules/shop/Bookings.tsx
@@ -5,24 +5,24 @@ import * as Crypto from 'crypto';
 import { Badge } from '@chakra-ui/react';
 
 const Bookings: React.FC = () => {
-  const tableHeaders: string[] = [
-    'Booking ID',
-    'Customer',
-    'Assigned Branch',
-    'Date and time',
-    'Status',
-    '',
-  ];
-  const tableData: BookingData[] = [
-    {
-      id: 12345,
-      bookingNum: Crypto.createHash('sha256').digest('hex').substring(0, 7),
-      customerName: 'Harrison Ford',
-      assignedBranch: 'U-Wash Escario',
-      bookingDate: '01/01/2023 00:00:00',
-      status: 'Completed',
-    },
-  ];
+  // const tableHeaders: string[] = [
+  //   'Booking ID',
+  //   'Customer',
+  //   'Assigned Branch',
+  //   'Date and time',
+  //   'Status',
+  //   '',
+  // ];
+  // const tableData: BookingData[] = [
+  //   {
+  //     id: 12345,
+  //     bookingNum: Crypto.createHash('sha256').digest('hex').substring(0, 7),
+  //     customerName: 'Harrison Ford',
+  //     assignedBranch: 'U-Wash Escario',
+  //     bookingDate: '01/01/2023 00:00:00',
+  //     status: 'Completed',
+  //   },
+  // ];
 
   return (
     <div>
